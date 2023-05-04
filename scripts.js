52ff324f2c7bde45de1010cfbdcdf0be7afbab01
RyanNolan1 <114003965+RyanNolan1@users.noreply.github.com>
2023-05-04 07:52:01 +0100

    this will create one square of the grid with javascript

---
+++ scripts.js
@@ -0,0 +1,9 @@
+let div = document.createElement("div");
+div.style.width = "100px";
+div.style.height = "100px";
+div.style.background = "red";
+div.style.color = "white";
+div.innerHTML = "Hello";
+
+document.body.appendChild(div);
+
