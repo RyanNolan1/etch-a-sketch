4dde1e4cc6e72315c8141b3a181c149774dc6ab2
RyanNolan1 <114003965+RyanNolan1@users.noreply.github.com>
2023-05-04 07:57:04 +0100

    this will get squares showing 16 times on the page

--- scripts.js
+++ scripts.js
@@ -1,9 +1,9 @@
+for (let i = 0; i < 16; i++) {
 let div = document.createElement("div");
 div.style.width = "100px";
 div.style.height = "100px";
 div.style.background = "red";
 div.style.color = "white";
-div.innerHTML = "Hello";
-
+div.style.margin = '10px'
 document.body.appendChild(div);
-
+}
