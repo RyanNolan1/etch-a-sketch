130606dbd7c8b84129f70648d6cf454ec77a4f63
RyanNolan1 <114003965+RyanNolan1@users.noreply.github.com>
2023-05-05 18:39:39 +0100

    this will change the size of the grid depending on the input from the user

--- scripts.js
+++ scripts.js
@@ -1,25 +1,34 @@
-let columns = 16;
-let rows = 16;
+getGrid(16, 16);
 
-const gridContainer = document.getElementById("grid-container");
-let grid = document.createElement("div");
-grid.className = "grid";
-for (let i = 0; i < columns; ++i) {
-  let column = document.createElement("div");
-  column.className = "column";
-  for (let j = 0; j < rows; ++j) {
-    let row = document.createElement("div");
-    row.className = "row";
+function getGrid(columns, rows) {
+  const gridContainer = document.getElementById("grid-container");
 
-    column.appendChild(row);
+  let grid = document.createElement("div");
+  grid.className = "grid";
+  for (let i = 0; i < columns; ++i) {
+    let column = document.createElement("div");
+    column.className = "column";
+    for (let j = 0; j < rows; ++j) {
+      let row = document.createElement("div");
+      row.className = "row";
+
+      column.appendChild(row);
+    }
+    grid.appendChild(column);
   }
-  grid.appendChild(column);
-}
-gridContainer.appendChild(grid);
+  gridContainer.appendChild(grid);
 
-const rowLoop = document.getElementsByClassName('row');
-for (let i = 0; i < rowLoop.length; i++) {
-    rowLoop[i].addEventListener("mouseover", function() {
-        rowLoop[i].style.backgroundColor = "black";
-  });
+  const rowLoop = document.getElementsByClassName("row");
+  for (let i = 0; i < rowLoop.length; i++) {
+    rowLoop[i].addEventListener("mouseover", function () {
+      rowLoop[i].style.backgroundColor = "red";
+    });
+  }
+}
+
+function changeGridSize() {
+  const clearContainer = document.getElementById("grid-container");
+  clearContainer.innerHTML = "";
+  let size = prompt("Please enter a number between 1 and 100");
+  getGrid(size, size);
 }
